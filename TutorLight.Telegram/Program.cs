e360b750f4e6b564ad4d77ceca27510b6f7b69fa
Monnoroch <sidelnikov.ns@gmail.com>
2021-08-14 21:20:52 +0300

    commit

--- TutorLight.Telegram/Program.cs
+++ TutorLight.Telegram/Program.cs
@@ -8,7 +8,7 @@ namespace TutorLight.Telegram
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            var botClient = new TelegramBotClient("1967940705:AAEqcSmYKP1tXz8JqT-LQOTYwEPYzcikrKg");
+            var botClient = new TelegramBotClient("token");
             var me = botClient.GetMeAsync().Result;
             Console.WriteLine(
                 $"Hello, World! I am user {me.Id} and my name is {me.FirstName}."
